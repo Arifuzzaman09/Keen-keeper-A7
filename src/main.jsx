be43ab3ae8fdc6ef62ad28ccccd545d6b3b8b347
mdarifuzzaman0916-dev <mdarifuzzaman0916@gmail.com>
2026-04-15 17:46:30 +0600

    added tailwind and daysi

---
+++ src/main.jsx
@@ -0,0 +1,35 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { createBrowserRouter } from 'react-router'
+import { RouterProvider } from 'react-router/dom'
+import RootLoyout from './Layout/RootLoyout'
+import "./index.css"
+const router = createBrowserRouter(
+  [
+    {
+      path:"/",
+      element: <RootLoyout></RootLoyout>,
+      children:[
+      {
+        path:"/",
+        element: <h2>Home</h2> 
+      },
+      {
+        path:"/timeline",
+        element: <h2>TimeLime</h2> 
+      },
+      {
+        path:"/stats",
+        element: <h2>Stats</h2> 
+      }
+      ],
+      errorElement: <h2>This page is not found</h2>
+    }
+  ]
+)
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+)
